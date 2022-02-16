31a878150077773783a6580095b927da88dba864
Ruihua NIU <ruihua.niu@hotmail.com>
2022-02-16 21:59:37 +1100

    fix: date

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <div className="footer-container">
             <a href="https://github.com/ruihuaniu/search-what-you-want" className="footer-icon"><img src="/images/github.png" alt="github icon" />  </a>
-            <p>`Copyright@ ${new Date().getFullYear} Ruihua All Rights Reserved`</p>
+            <p>{`Copyright@ ${new Date().getFullYear()} Ruihua All Rights Reserved`}</p>
         </div>
     )
 }
